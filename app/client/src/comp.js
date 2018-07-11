85a00d3117b404d4c395a3a700f3a98238671016
Thomas DeFeo <thomasmdefeo@gmail.com>
2018-07-10 17:25:25 -0700

    add rack-cors gem and reconfig application.rb api call in Comp successfully getting shirt list

--- app/client/src/comp.js
+++ app/client/src/comp.js
@@ -1,9 +1,25 @@
 import React, {Component} from 'react';
+import axios from 'axios';
 
 export default class Comp extends Component {
+  state = {
+    shirts: [],
+  }
+  componentDidMount() {
+    axios.get('http://localhost:3001/api/v1/shirts.json')
+        .then(response => {
+            console.log(response)
+            this.setState({
+                shirts: response.data
+            })
+        })
+        .catch(error => console.log(error))
+  }
+
   render() {
     return(
-      <p>user</p>
+      <p>sdf</p>
+
     );
   }
 }
